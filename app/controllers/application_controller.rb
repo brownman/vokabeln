87402340b57ba812c3b6f488431712e684cd557b
Tim Baumann <tim@timbaumann.info>
2011-01-08 13:46:36 +0100

    404 page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  rescue_from ActiveRecord::RecordNotFound, :with => :not_found
+  
+  def not_found
+    @title = "404"
+    render "application/not_found", :status => 404
+  end
 end
