21b258ca1fb6c228abbe4529a60f30074119c618
Tim Baumann <tim@timbaumann.info>
2011-02-27 14:41:27 +0100

    Unit tests

--- test/unit/lesson_test.rb
+++ test/unit/lesson_test.rb
@@ -1,8 +1,18 @@
 require 'test_helper'
 
 class LessonTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  def new_lesson(attributes = {})
+    attributes[:name] ||= "Welcome back to School!"
+    lesson = Lesson.new(attributes)
+    lesson.valid? # run validations
+    lesson
+  end
+
+  def test_valid
+    assert new_lesson.valid?
+  end
+
+  def test_require_name
+    assert_equal ["can't be blank"], new_lesson(:name => "").errors[:name]
   end
 end
--- test/unit/vocable_test.rb
+++ test/unit/vocable_test.rb
@@ -1,8 +1,51 @@
+# coding: utf-8
+
 require 'test_helper'
 
 class VocableTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  def new_vocable(attributes = {})
+    attributes[:lesson_id] = lessons(:numbers).id unless attributes.key? :lesson_id
+    attributes[:foreign] ||= "Cat"
+    attributes[:translation] ||= "Katze"
+    vocable = Vocable.new(attributes)
+    vocable.valid? # run validations
+    vocable
+  end
+
+  def test_valid
+    assert new_vocable.valid?
+  end
+
+  def test_require_lesson
+    assert_equal ["can't be blank"], new_vocable(:lesson_id => nil).errors[:lesson_id]
+  end
+
+  def test_require_foreign
+    assert_equal ["can't be blank"], new_vocable(:foreign => "").errors[:foreign]
+  end
+
+  def test_require_translation
+    assert_equal ["can't be blank"], new_vocable(:translation => "").errors[:translation]
+  end
+
+  def test_search_by_foreign
+    results = Vocable.search("four")
+    assert results.include?(vocables(:four))
+    assert results.include?(vocables(:fourteen))
+  end
+
+  def test_search_by_translation
+    results = Vocable.search("vier")
+    assert results.include?(vocables(:four))
+    assert results.include?(vocables(:fourteen))
+  end
+
+  def test_check_foreign
+    vocable = vocables(:eleven)
+    assert_equal true, vocable.check_foreign("eleven")
+    assert_equal false, vocable.check_foreign("noidea")
+    assert_equal 2, vocable.attempts.size
+    assert_equal true, vocable.attempts[0].success
+    assert_equal false, vocable.attempts[1].success
   end
 end
